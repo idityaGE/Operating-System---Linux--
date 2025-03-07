5de628ba977788c58ae5608e9213e6efbed46f67
idityaGE <am44910606@gmail.com>
2025-03-07 20:47:42 +0530

    find script is done

--- Script/LE3/find.sh
+++ Script/LE3/find.sh
@@ -4,20 +4,29 @@
 # out files/ directory/ or a string representing some portion of directory or file names.
 # I will be happy to see if you can also implement regular expressions.
 
+if [[ $# -eq 0 ]]; then
+  echo "Search arguments is not provided !"
+  echo "Usage: $0 <args> [directory]"
+  exit 1
+fi
+
 args=$1
-if [[ -z $args ]]; then
-  echo "Please provide the arguments to find !"
+start_dir="${2:-$PWD}"
+
+if [[ ! -d "$start_dir" ]]; then
+  echo "Error: '$start_dir' is not a valid directory"
   exit 1
 fi
 
 process_directory() {
   local current_path="$1"
+  local file
   # echo -e "Searching in : $current_path\n"
 
   for file in "$current_path"/*; do
     [[ -e $file ]] || continue
 
-    if echo "$file" | grep -i "$args"; then
+    if echo "$file" | grep -q -i "$args"; then
       echo "$file"
     fi
 
@@ -27,4 +36,5 @@ process_directory() {
   done
 }
 
-process_directory "$PWD"
+echo "Searching for '$args' in '$start_dir'..."
+process_directory "$start_dir"
